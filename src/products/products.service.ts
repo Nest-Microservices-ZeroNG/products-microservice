8a0f63f2ff894393f0c9c16edf2f265a3e80b3df
Jhoset NG <adm_dng@hotmail.com>
2024-07-20 14:35:03 -0500

    Implement Products App (RESTful API) - SQLite - Prisma - NestJS/Express

---
+++ src/products/products.service.ts
@@ -0,0 +1,77 @@
+import {
+  Injectable,
+  Logger,
+  NotFoundException,
+  OnModuleInit,
+} from '@nestjs/common';
+import { CreateProductDto } from './dto/create-product.dto';
+import { UpdateProductDto } from './dto/update-product.dto';
+import { PrismaClient } from '@prisma/client';
+import { PaginationDto } from '../common';
+
+@Injectable()
+export class ProductsService extends PrismaClient implements OnModuleInit {
+  private readonly logger = new Logger('ProductsService');
+
+  onModuleInit(): any {
+    this.$connect();
+    this.logger.log('Database Sqlite Connected!');
+  }
+
+  create(createProductDto: CreateProductDto) {
+    return this.product.create({
+      data: { ...createProductDto },
+    });
+  }
+
+  async findAll(paginationDto: PaginationDto) {
+    const { page, limit } = paginationDto;
+
+    const total = await this.product.count({ where: { available: true } });
+    const pages = Math.ceil(total / limit);
+
+    return {
+      data: await this.product.findMany({
+        skip: (page - 1) * limit,
+        take: limit,
+        where: { available: true },
+      }),
+      meta: {
+        page,
+        limit,
+        total,
+        pages,
+      },
+    };
+  }
+
+  async findOne(id: number) {
+    const product = await this.product.findUnique({
+      where: { id, available: true },
+    });
+    if (!product)
+      throw new NotFoundException(`Product with ID ${id} not found`);
+    return product;
+  }
+
+  async update(id: number, updateProductDto: UpdateProductDto) {
+    await this.findOne(id);
+    return this.product.update({
+      where: { id },
+      data: { ...updateProductDto },
+    });
+  }
+
+  async remove(id: number) {
+    await this.findOne(id);
+    //return this.product.delete({
+    //  where: { id },
+    //});
+    return this.product.update({
+      where: { id },
+      data: {
+        available: false,
+      },
+    });
+  }
+}
