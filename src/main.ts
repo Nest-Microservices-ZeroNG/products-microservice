75183b69a9cdbebd0fe80ca2d7d6c554850aeb08
Jhoset NG <adm_dng@hotmail.com>
2024-08-08 10:26:25 -0500

    Added dockerfile

--- src/main.ts
+++ src/main.ts
@@ -7,8 +7,6 @@ import { MicroserviceOptions, Transport } from '@nestjs/microservices';
 async function bootstrap() {
   const logger = new Logger('Main');
 
-  console.log(envs.natsServers);
-
   const app = await NestFactory.createMicroservice<MicroserviceOptions>(
     AppModule,
     {
